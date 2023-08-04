655e99171de17fd0e17ef39745e0cbdc713cfc58
Zain Raza <sraza@ptc.com>
2023-08-04 10:38:24 -0400

    add first rust 'hello, world'/Applications/OpenLens.app/Contents/MacOS/OpenLens' -p '41ab651cf37f4038aaedf61db04866a7 + JSON.stringify(process.env) + 41ab651cf37f4038aaedf61db04866a7' program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![allow(unused)]  // used to ignore a compiler warning
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // 
+    println!("What is your name?");  // "!" = this function is a macro
+    // every var declaration starts with "let"
+    let mut name: String = String::new();  // instantiates a String
+    let greeting: &str = "Nice to meet you"; // another way to init a string
+
+    /* Interacting with User Input */
+    // we make a call to collect a ref to a string (so that read_line() will save values directly to the "name" param)
+    io::stdin().read_line(&mut name)  // returns a result enum - Ok, or Error
+        .expect("Didn't Receive Input");   // error message w/ built-in exception handling
+
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
